d10485b2fe54338b155168fd500721b92dc8bd56
rgreguiuqeo <dyon.p03@gmail.com>
2024-01-10 12:21:41 +0100

    added simple visualisation dif to main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from battery import Battery
 from house import House
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import tkinter
+import matplotlib
+matplotlib.use('TkAgg')
+
 
 class Smartgrid():
 
     def __init__(self, district):
-    
+        
         self.batteries = {}
         self.houses = {}
         
         filename_batteries = f"Huizen&Batterijen/district_{district}/district-{district}_batteries.csv"
         filename_houses = f"Huizen&Batterijen//district_{district}/district-{district}_houses.csv"
         
-        self.inlezer(filename_batteries, filename_houses)
-
-    def inlezer(self, filename_batteries, filename_houses):
+        self.csv_reader(filename_batteries, filename_houses)
+        
+        self.visualisation()
+    
+    def csv_reader(self, filename_batteries, filename_houses):
+        """ initializes objecten met csv data"""
     
         with open(filename_batteries, 'r') as file:
             csv_reader = csv.reader(file)
@@ -38,6 +47,41 @@ class Smartgrid():
                 capacity = line[2]
                 self.houses[house_id] = House(battery_id, int(x), int(y), float(capacity))
                 house_id += 1
+                
+    def extract_cords_and_capacity(self, dictionary):
+        """ extracts cords and capacity from house or battery objects"""
+        x_list = []
+        y_list = []
+        capacity_list = []
+        
+        for key, value in dictionary.items():
+            x = value.x
+            y = value.y
+            capacity = value.capacity
+            
+            x_list.append(x)
+            y_list.append(y)
+            capacity_list.append(capacity)
+        
+        return x_list, y_list, capacity_list
+                
+    def visualisation(self):
+        """ creates a scatter plot with extracted cords and capacity"""
+        
+        self.batteries_lijst = self.extract_cords_and_capacity(self.batteries)
+        self.houses_lijst = self.extract_cords_and_capacity(self.houses)
+        
+        x_batteries = self.batteries_lijst[0]
+        y_batteries = self.batteries_lijst[1]
+        
+        x_houses = self.houses_lijst[0]
+        y_houses = self.houses_lijst[1]
+        
+        plt.scatter(x_batteries, y_batteries)
+        plt.scatter(x_houses, y_houses)
+        
+        plt.grid(True, dashes=(1, 1), linewidth=0.5)
+        plt.show()
             
 if __name__ == "__main__":
     smartgrid = Smartgrid("1")
