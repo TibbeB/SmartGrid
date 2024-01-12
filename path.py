246a4b1b8efc22e28b2420ae38e82caaa72a2f2a
patchy69 <patrickdkools@gmail.com>
2024-01-12 11:21:01 +0100

    file which connects houses to batteries

---
+++ path.py
@@ -0,0 +1,50 @@
+from cable import Cable
+
+def path():
+    """
+    Args: dict_connections (dictionary), cables (dictionary)
+    
+    Return: None"""
+    def __init__(self, dict_connections, cables):
+        self.dict_connections = dict_connections
+        self.cables = cables
+        
+        
+    def x_y_path(self):
+    
+        # iterates trough all batteries in the dictionaries
+        for battery in self.dict_connections:
+            x_battery = battery.x
+            y_battery = battery.y
+            
+            connections = self.dict_connections[battery]
+            
+            # iterates trough all houses at a specific battery
+            for houses in connections:
+                cable = self.cables[houses.identification]
+                
+                x_house = houses.x
+                y_house = houses.y
+                
+                x_distance = x_battery - x_house
+                y_distance = y_battery - y_house
+                
+                # iterates trough all steps from the house to the battery
+                # determines left or right
+                for x_steps in range(abs(x_distance)):
+                    if x_distance < 0:
+                        cable.left()
+                        
+                    else:
+                        cable.right()
+                
+                # iterates trough all steps from the house to the battery
+                # determines up or down
+                for y_steps in range(abs(y_dstance)):
+                    if y_distance < 0:
+                        cable.down()
+                        
+                    else:
+                        cable.up()
+             
+       
