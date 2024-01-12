dd390b6bb487eaaec1a9828a2473279375e79865
patchy69 <patrickdkools@gmail.com>
2024-01-12 11:31:02 +0100

    Small adjustment to path.py

--- path.py
+++ path.py
@@ -1,43 +1,32 @@
-from cable import Cable
+        
+def x_y_path(self, dict_connections):
 
-def path():
-    """
-    Args: dict_connections (dictionary), cables (dictionary)
-    
-    Return: None"""
-    def __init__(self, dict_connections, cables):
-        self.dict_connections = dict_connections
-        self.cables = cables
+    # iterates trough all batteries in the dictionaries
+    for battery in self.dict_connections:
+        x_battery = battery.x
+        y_battery = battery.y
         
+        connections = self.dict_connections[battery]
         
-    def x_y_path(self):
-    
-        # iterates trough all batteries in the dictionaries
-        for battery in self.dict_connections:
-            x_battery = battery.x
-            y_battery = battery.y
+        # iterates trough all houses at a specific battery
+        for houses in connections:
+            cable = self.cables[houses.identification]
             
-            connections = self.dict_connections[battery]
+            x_house = houses.x
+            y_house = houses.y
             
-            # iterates trough all houses at a specific battery
-            for houses in connections:
-                cable = self.cables[houses.identification]
-                
-                x_house = houses.x
-                y_house = houses.y
-                
-                x_distance = x_battery - x_house
-                y_distance = y_battery - y_house
-                
-                # iterates trough all steps from the house to the battery
-                # determines left or right
-                for x_steps in range(abs(x_distance)):
-                    if x_distance < 0:
-                        cable.left()
-                        
-                    else:
-                        cable.right()
-                
+            x_distance = x_battery - x_house
+            y_distance = y_battery - y_house
+            
+            # iterates trough all steps from the house to the battery
+            # determines left or right
+            for x_steps in range(abs(x_distance)):
+                if x_distance < 0:
+                    cable.left()
+                    
+                else:
+                    cable.right()
+            
                 # iterates trough all steps from the house to the battery
                 # determines up or down
                 for y_steps in range(abs(y_dstance)):
